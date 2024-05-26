1cf0567799bf702f8732bda5b06bad37ce6b7d3d
Gilles <102923484+gillesdeperetti@users.noreply.github.com>
2024-05-26 14:21:35 +0200

    mail server test

--- server/app.js
+++ server/app.js
@@ -54,6 +54,7 @@ app.post("/send-email", async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-    console.log("http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
