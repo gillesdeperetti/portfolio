5e137ce7f42fff4a49b6b38b8946b7cdf039e7b5
Gilles <102923484+gillesdeperetti@users.noreply.github.com>
2024-05-26 14:12:59 +0200

    Update Vite config to listen on all interfaces and use environment port

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, 
+    host: '0.0.0.0', 
     port: process.env.PORT || 3000, 
   },
   preview: {
-    host: true, 
+    host: '0.0.0.0', 
     port: process.env.PORT || 3000, 
   },
 })
